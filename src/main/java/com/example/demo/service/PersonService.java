cce365f48aa26ab1ffaa8c0bdc6fc5c58e577b72
Rey Matsunaga <matsunagar97@gmail.com>
2019-05-06 19:24:23 -0700

    implemented get and post requests

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -2,16 +2,27 @@ package com.example.demo.service;
 
 import com.example.demo.dao.PersonDAO;
 import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class PersonService {
 
     private final PersonDAO personDAO;
 
-    public PersonService(PersonDAO personDAO) {
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDAO personDAO) {
         this.personDAO = personDAO;
     }
 
     public int addPerson(Person person) {
         return personDAO.insertPerson(person);
+    }
+
+    public List<Person> getAllPeople() {
+        return personDAO.selectAllPeople();
     }
 }

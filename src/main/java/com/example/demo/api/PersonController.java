04690e1b82317c6b404ae1968db487786886ddee
Rey Matsunaga <matsunagar97@gmail.com>
2019-05-10 13:02:57 -0700

    more swagger configuration

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -2,6 +2,10 @@ package com.example.demo.api;
 
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,28 +26,58 @@ public class PersonController {
     }
 
     @PostMapping
+    @ApiOperation(value = "adds person to the database")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "success!")
+            }
+    )
     public void addPerson(@Valid @NotNull @RequestBody Person person) {
         personService.addPerson(person);
     }
 
 
     @GetMapping
+    @ApiOperation(value = "returns list of all people")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "success!")
+            }
+    )
     public List<Person> selectAllPeople() {
         return personService.selectAllPeople();
     }
 
     @GetMapping(path = "{id}")
+    @ApiOperation(value = "returns person with provided id")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "success!")
+            }
+    )
     public Person selectPersonById(@PathVariable("id") UUID id) {
         return personService.selectPersonById(id)
                 .orElse(null);
     }
 
     @DeleteMapping(path = "{id}")
+    @ApiOperation(value = "deletes person with provided id from database")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "success!")
+            }
+    )
     public void deletePersonById(@PathVariable("id") UUID id) {
         personService.deletePerson(id);
     }
 
     @PutMapping(path = "{id}")
+    @ApiOperation(value = "updates person with provided id with information provided")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "success!")
+            }
+    )
     public void updatePerson(@PathVariable("id") UUID id, @Valid @NotNull @RequestBody Person personToUpdate) {
         personService.updatePerson(id, personToUpdate);
     }
--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,6 +21,22 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
                 .paths(regex("/api/v1/person.*"))
-                .build();
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Spring Boot Person API",
+                "Spring Boot Swagger Example API",
+                "1.0",
+                "Terms of Service",
+                "Rey Matsunaga",
+                "Apache License Version 2.0",
+                "localhost:8080"
+        );
+
+        return apiInfo;
+
     }
 }
